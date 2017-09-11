af92a5056a7538360ad18498e684494bab591350
Simon Wagner <wagnersi@in.tum.de>
2017-09-11 13:48:57 +0200

    Prepare for integration with libmoon
    lwip-dpdk will no longer be useable as standalone

--- build.sh
+++ build.sh
@@ -2,10 +2,5 @@
 
 NUM_CPUS=$(cat /proc/cpuinfo  | grep "processor\\s: " | wc -l)
 
-(
-cd ./dpdk
-make -j $NUM_CPUS install T=x86_64-native-linuxapp-gcc EXTRA_CFLAGS='-g -O0'
-)
-
 make -f lwip.makefile
 make
